aa5067973698b41ae512bc7f2f2aa6423d133bbe
ndjaka <eugene.ndjaka@kaeyros-analytics.de>
2021-12-20 16:29:38 +0100

    dynamic project pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,7 @@
-/**
-* Template Name: DevFolio - v4.7.1
-* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
+
+  
 
   /**
    * Easy selector helper function
